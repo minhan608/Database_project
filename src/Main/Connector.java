cdb283555fcd3c451ee73fb984eef1929cab9959
Nguyen Minh An <anmin.nguyen608@gmail.com>
2021-04-24 20:20:07 +0700

    full demo

--- src/Main/Connector.java
+++ src/Main/Connector.java
@@ -17,7 +17,7 @@ public class Connector {
         try {
 
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital?useSSL=true", "root", "Sajeeb");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/patient_tracker?useSSL=true", "root", "");
 //            JOptionPane.showMessageDialog(null, "Connection Established");
             return connection;
         } catch (ClassNotFoundException | SQLException ex) {
